304d1ff7c598dabc2fe2fd03eb8934b8239e77c5
yohannNDB <91455596+yohannNDB@users.noreply.github.com>
2023-02-11 15:39:40 +0100

    1

--- app.py
+++ app.py
@@ -8,12 +8,21 @@ def index():
 
 
 @app.route('/QUINSI1')
+def game1():
+    return render_template('game.html')
+
+@app.route('/QUINSI2')
+def game2():
+    return render_template('game2.html')
+
+@app.route('/QUINSI3')
 def player1():
     return render_template('player1.html')
 
-@app.route('/QUINSI2')
+@app.route('/QUINSI4')
 def player2():
     return render_template('player2.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
